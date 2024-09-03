e428e851c33b73ea8c525c211fa3477eb45d5e46
carlapascobello <maspascobello@su.edu.ph>
2024-09-03 10:19:48 +0800

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 function displayPoem(response) {
-  console.log("poem generated");
-
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -12,23 +10,18 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
-  // build API URL
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "cfdo22bb3e4ct447ead942a949a20eb3";
   let prompt = `User instructions: Generate a Spanish poem about ${instructionsInput.value}`;
   let context =
-    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML format and separate each line with a <br />. Remove the '```html'. Make sure to follow the user instructions. Also, include the English translation of the peom below the Spanish version, with a <br /> before it. Add a <strong> element before and after the spanish version. Sign the poem with 'SheCodes AI' inside an <em> element. Please, remember to add a <br /> element before the english version. All another <br /> tag right before the <em> element.";
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML format and separate each line with a <br /> tag. Remove the '```html'. Make sure to follow the user instructions. Also, include the English translation of the peom below the Spanish version. Add a <strong> element before and after the Spanish version only. Please, add another two <br /> elements after the Spanish version to separate the two languages. At the bottom, sign the poem with 'SheCodes AI' inside an <em> element. Please, add another two <br /> tags right before the <em> element. Do this consistenly, please.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log(apiUrl);
-  console.log("Generating poem...");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
-  // Make a call to the API
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⌛️ Generating a Spanish poem about ${instructionsInput.value}...</div>`;
 
   axios.get(apiUrl).then(displayPoem);
-
-  // Display the generated poem
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
